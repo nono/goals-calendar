b418ecacdcbc824691c02d999da6fa2fd62edf8c
Bruno Michel <bmichel@menfin.info>
2010-01-09 00:13:32 +0100

    Goldorak GO!

--- goals-calendar.go
+++ goals-calendar.go
@@ -1,25 +1,34 @@
 package main
 
-import (
-	"gostache"
-	"nogo"
-	"web"
-)
-
-var m *nogo.Model
-
-func hello(ctx *web.Context, val string) {
-	filename := "app/templates/hello.mustache"
-	// m.Set("toto", "foobar")
-	world := map[string]string{"name": m.Get(val)}
-	output, _ := gostache.RenderFile(filename, world)
-	ctx.WriteString(output)
-}
+import "goldorak"
 
 func main() {
-	nogo.Initialize()
-	nogo.Get("/(.*)", hello)
-	nogo.Start()
-	m = nogo.NewModel("calendar")
+	/******************/
+	/* Initialization */
+	/******************/
+	goldorak.Initialize("config.json")
+	// Calendar := goldorak.NewModel("calendar")
+
+	/***********/
+	/* Actions */
+	/***********/
+
+	// Hello world
+	goldorak.Get("/hello", func(action goldorak.Action) {
+		action.Assign("name", "World!")
+		//action.Assign("name", Calendar.Find("hello").Get("world"))
+		action.Layout("")
+		action.Render("hello")
+	});
+
+	// Show a calendar
+	goldorak.Get("/calendars/(.*)", func(action goldorak.Action) {
+		action.Render("calendar")
+	});
+
+	/************/
+	/* Let's go */
+	/************/
+	goldorak.Start()
 }
 
