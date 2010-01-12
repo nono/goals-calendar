71e005638bed8433ff67bd8c0b9516435badc28c
Bruno Michel <bmichel@menfin.info>
2010-01-12 23:09:41 +0100

    Use the new API for models

--- goals-calendar.go
+++ goals-calendar.go
@@ -1,13 +1,18 @@
 package main
 
-import "goldorak"
+import (
+	"goldorak"
+	"strconv"
+)
+
 
 func main() {
 	/******************/
 	/* Initialization */
 	/******************/
 	goldorak.Initialize("config.json")
-	//calendar := goldorak.NewModel("calendar")
+	conn := goldorak.Connect()
+	calendar := conn.NewModel("calendar")
 
 	/***********/
 	/* Actions */
@@ -22,15 +27,24 @@ func main() {
 
 	// Hello world
 	goldorak.Get("/hello", func(action *goldorak.Action, params []string) {
-		action.Assign("name", params[0])
-		//action.Assign("name", calendar.Find("hello").Get("world"))
-		//action.NoLayout()
+		action.Assign("name", "world")
+		action.NoLayout()
 		action.Template("hello")
 	});
 
 	// Show a calendar
-	goldorak.Get("/calendars/.*", func(action *goldorak.Action, params []string) {
-		action.Template("calendar")
+	goldorak.Get("/.*(/[0-9]+/[0-9]+)?", func(action *goldorak.Action, params []string) {
+		cal     := calendar.Find(params[0])
+		if cal != nil {
+			// Show the calendar
+			year, _ := strconv.Atoi(params[0])
+			month,_ := strconv.Atoi(params[1])
+			action.Assign("name", cal.Get("title"))
+			action.Assign("not_used", string(year + month))
+			action.Template("calendar")
+		} else {
+			// TODO create a new calendar
+		}
 	});
 
 	/************/
