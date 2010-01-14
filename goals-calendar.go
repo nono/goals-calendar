279debc6e1d297aebe1543aa48cd409d8ff8e9cc
Bruno Michel <bmichel@menfin.info>
2010-01-14 01:18:48 +0100

    WIP for show/create calendars

--- goals-calendar.go
+++ goals-calendar.go
@@ -4,9 +4,48 @@ import (
 	"fmt"
 	"goldorak"
 	"strconv"
+	"strings"
 	"time"
 )
 
+// TODO create a type calendar
+var goal *goldorak.Model
+
+
+// Show the calendar
+func showCalendar(action *goldorak.Action, goal *goldorak.Instance, params []string) {
+	var year, month int
+	if (len(params) > 2) {
+		year, _ = strconv.Atoi(params[1])
+		month,_ = strconv.Atoi(params[2])
+	} else {
+		now := time.LocalTime()
+		year, month = int(now.Year), now.Month
+	}
+	action.Assign("name", goal.Get("title"))
+	action.Assign("year", fmt.Sprint("%04d", year))
+	action.Assign("month", fmt.Sprint("%02d", month))
+	action.Assign("prev_url", "/" + params[0]) // FIXME
+	action.Assign("next_url", "/" + params[0]) // FIXME
+	action.Assign("rows", "") // FIXME
+	action.Assign("current", goal.Get("current") + " jours") // TODO Pluralize
+	action.Assign("longest", goal.Get("longest") + " jours") // TODO Pluralize
+	action.Template("calendar")
+}
+
+// Form for creating a new goal
+func newGoal(action *goldorak.Action, param string) {
+	action.Assign("name", param)
+	action.Template("new_calendar")
+}
+
+// Create a goal
+func createGoal(action *goldorak.Action, name string) {
+	s := goldorak.Parameterize(name)
+	g := goal.Create(s)
+	g.Set("name", name)
+	action.Redirect("/" + s)
+}
 
 func main() {
 	/******************/
@@ -14,7 +53,7 @@ func main() {
 	/******************/
 	goldorak.Initialize("config.json")
 	conn := goldorak.Connect()
-	calendar := conn.NewModel("calendar")
+	goal  = conn.NewModel("goal")
 
 	/***********/
 	/* Actions */
@@ -29,33 +68,22 @@ func main() {
 
 	// Show a calendar
 	goldorak.Get("/.*(/[0-9]+/[0-9]+)?", func(action *goldorak.Action, params []string) {
-		var year int64
-		var month int
-		cal := calendar.Find(params[0])
-		if cal != nil {
-			// Show the calendar
-			if (len(params) > 2) {
-				year, _ = strconv.Atoi64(params[0])
-				month,_ = strconv.Atoi(params[1])
-			} else {
-				now := time.LocalTime()
-				year, month = now.Year, now.Month
-			}
-			action.Assign("name", cal.Get("title"))
-			action.Assign("year", fmt.Sprint("%04d", year))
-			action.Assign("month", fmt.Sprint("%02d", month))
-			action.Template("calendar")
+		g := goal.Find(params[0])
+		if g != nil {
+			showCalendar(action, g, params)
 		} else {
-			action.Assign("name", params[0])
-			action.Template("new_calendar")
+			newGoal(action, params[0])
 		}
 	});
 
 	// Create a calendar
 	goldorak.Post("/calendars", func(action *goldorak.Action, params []string) {
-		slug := goldorak.Parameterize(params[0])
-		cal := calendar.Create(slug)
-		action.Redirect("/" + cal.Param)
+		name := strings.TrimSpace(params[0])
+		if name != "" {
+			createGoal(action, name)
+		} else {
+			newGoal(action, "")
+		}
 	});
 
 	/************/
